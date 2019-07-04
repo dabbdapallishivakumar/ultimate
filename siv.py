7a83c7b235e64d619febc2b0e28971c48be35e01
dabbdapallishivakumar <52519205+dabbdapallishivakumar@users.noreply.github.com>
2019-07-04 12:21:31 +0530

    Update and rename siv.py to siv1.py

--- siv.py
+++
@@ -1,6 +0,0 @@
-d=int(input("enter the number"))
-mod=d%2
-if mod>0:
-   print("odd")
-   else:
-      print("even")
---
+++ siv1.py
@@ -0,0 +1,5 @@
+d=input("enter the char")
+if(d=='a' or d=='A' or d=='i' or d=='I' or d=='e' or d=='E' or d=='u' or d=='U' or d=='o' or d=='O'):
+   print(d, "as a vewol")
+   else:
+      print(d,"const")

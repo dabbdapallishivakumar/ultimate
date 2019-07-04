e5ff57b587570ea18fa2aac1bca7e78f5974940f
dabbdapallishivakumar <52519205+dabbdapallishivakumar@users.noreply.github.com>
2019-07-04 09:36:19 +0530

    Update siva.py

--- siva.py
+++ siva.py
@@ -2,7 +2,7 @@ n=float(input("enter number"))
 if n>=0:
    if n==0:
       print("ZERo")
-      else:
-         print("POSITIVE")
-         else:
-            print("NEGATIVE")
+    else:
+       print("POSITIVE")
+ else:
+    print("NEGATIVE")
